20e82545d425ee1776b9498b4fa57ca85af08fb2
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-08-02 04:50:05 -0400

    Add higher z-index

--- app/components/section-number/index.tsx
+++ app/components/section-number/index.tsx
@@ -6,7 +6,7 @@ type SectionNumberProps = {
 
 const SectionNumber = ({ children }: SectionNumberProps) => {
   return (
-    <div className="relative w-full h-[140px] mt-[64px]">
+    <div className="relative w-full h-[140px] mt-[64px] z-10">
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[1px] h-[84px] bg-fem-cool-gray/25"></div>
       <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex justify-center items-center w-[56px] aspect-square border-[1px] border-fem-cool-gray/25 rounded-full text-fem-fluid-body font-black text-fem-cool-gray bg-white">
         {children}
