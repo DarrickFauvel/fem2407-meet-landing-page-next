e21f1bcbd1df19ce3ace1810191c401e84ac6006
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-08-04 05:38:11 -0400

    Create four image component

---
+++ app/components/four-images/index.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+import imageWomanInVideocall from "@/assets/images/desktop/image-woman-in-videocall.jpg";
+import imageWomenVideoChatting from "@/assets/images/desktop/image-women-videochatting.jpg";
+import imageMenInMeeting from "@/assets/images/desktop/image-men-in-meeting.jpg";
+import imageManTexting from "@/assets/images/desktop/image-man-texting.jpg";
+
+const FourImages = () => {
+  const images = [
+    {
+      src: imageWomanInVideocall,
+      alt: "woman video chatting",
+    },
+    {
+      src: imageWomenVideoChatting,
+      alt: "women video chatting",
+    },
+    {
+      src: imageMenInMeeting,
+      alt: "men in meeting",
+    },
+    {
+      src: imageManTexting,
+      alt: "man texting",
+    },
+  ];
+
+  const renderImages = images.map((image) => (
+    <Image
+      className="rounded-lg"
+      src={image.src}
+      width={151}
+      height={143}
+      alt={image.alt}
+      key={image.src}
+    />
+  ));
+
+  return (
+    <div className="mt-16 grid grid-cols-1 gap-6 min-[375px]:grid-cols-2 md:grid-cols-4">
+      {renderImages}
+    </div>
+  );
+};
+export default FourImages;
