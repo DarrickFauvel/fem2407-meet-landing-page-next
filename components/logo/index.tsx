e57d89634ba68da7028e8b3cd8fd5f51fb77071c
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-08-09 12:13:49 -0400

    Increase top margin for large screens

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <Image className="mb-[54px] mt-[48px]" src={imageLogo} alt="meet logo" />
+    <Image
+      className="mb-[54px] mt-[48px] lg:mt-[80px]"
+      src={imageLogo}
+      alt="meet logo"
+    />
   );
 };
 export default Logo;
