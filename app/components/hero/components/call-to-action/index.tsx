fb0d4169f8faca6f00b0752b8e05a66f82fbd462
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-08-10 06:00:14 -0400

    Set max character widths

--- app/components/hero/components/call-to-action/index.tsx
+++ app/components/hero/components/call-to-action/index.tsx
@@ -3,11 +3,11 @@ import Button from "@/components/logo/button";
 const CallToAction = () => {
   return (
     <div className="flex flex-col items-center gap-6 px-6 pt-12 lg:w-full lg:px-0">
-      <h1 className="text-balance text-center text-fem-fluid-h1 font-black leading-none text-fem-space-cadet">
+      <h1 className="max-w-[12ch] text-balance text-center text-fem-fluid-h1 font-black leading-none text-fem-space-cadet">
         Group Chat for Everyone
       </h1>
 
-      <p className="max-w-[45ch] text-center text-fem-fluid-body font-medium leading-fem-fluid-body text-fem-cool-gray sm:max-w-[54ch]">
+      <p className="max-w-[45ch] text-center text-fem-fluid-body font-medium leading-fem-fluid-body text-fem-cool-gray sm:max-w-[54ch] lg:max-w-[65ch]">
         Meet makes it easy to connect with others face-to-face virtually and
         collaborate across any device.
       </p>
